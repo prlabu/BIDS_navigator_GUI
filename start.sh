1e562ee4ea322827aa4f8a55ec69e861974d8b11
Latane Bullock <1prlabu1@gmail.com>
2023-05-05 14:36:39 -0400

    fix bmlnav typo

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # conda list -e > req.txt
 # conda create -n <environment-name> --file req.txt
 
-envname=bmlnav_test
+envname=bmlnav
 
 # if this environment doesn't exist, create one with req.txt
 if conda info --envs | grep -q $envname; then echo "$envname already exists"; else conda create -n $envname --file req.txt; fi
